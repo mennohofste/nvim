7ab9e36dce70df2e03544305817063086c6fd383
Menno <menno1337@gmail.com>
2024-03-26 11:56:25 +0200

    refactor: use `<C-y>` to accept completions

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,55 +1,12 @@
 return {
-  {
-    "L3MON4D3/LuaSnip",
-    keys = function()
-      return {}
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      local luasnip = require("luasnip")
-      local cmp = require("cmp")
-
-      opts.preselect = cmp.PreselectMode.None
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert })
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<CR>"] = cmp.config.disable,
-      })
-    end,
-  },
-  {
-    "echasnovski/mini.pairs",
-    opts = function(_, opts)
-      opts.mappings = {
-        ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\][%s]" },
-      }
-    end,
-  },
+  "hrsh7th/nvim-cmp",
+  opts = function(_, opts)
+    local cmp = require("cmp")
+    opts.mapping = cmp.mapping.preset.insert(vim.tbl_deep_extend("force", opts.mapping, {
+      ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+      ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+      ["<C-y>"] = cmp.mapping.confirm({ select = false }),
+      ["<CR>"] = cmp.config.disable,
+    }))
+  end,
 }
