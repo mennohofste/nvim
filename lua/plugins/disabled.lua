e6770babcd603ce6071d94b26213bc97874d8076
Menno <menno1337@gmail.com>
2024-04-30 13:01:29 +0300

    fix: disable `nvim-treesitter-context`
    After some simple testing it seems that this plugin is responsible for
    stuttering when scrolling. I like the features it brings, but smooth
    scrolling is more important for now. I can revisit in the future.

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,10 +1,8 @@
 return {
-  {
-    "akinsho/bufferline.nvim",
-    enabled = false,
-  },
-  {
-    "echasnovski/mini.pairs",
-    enabled = false,
-  },
+  -- Better awareness without a bufferline
+  { "akinsho/bufferline.nvim", enabled = false },
+  -- Less confusion when surrounding or editing
+  { "echasnovski/mini.pairs", enabled = false },
+  -- Stuttering when scrolling
+  { "nvim-treesitter/nvim-treesitter-context", enabled = false },
 }
