8a96224b4745cecb2fee4cad4e796e6b2f79f921
Menno <menno1337@gmail.com>
2024-04-30 12:59:57 +0300

    feat: remove `ruff_lsp` specific options
    `ruff_lsp` has been replaced with the more modern `ruff` lsp, see
    https://github.com/LazyVim/LazyVim/pull/3016.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,7 +7,6 @@ return {
       servers = {
         -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = { settings = { python = { pythonPath = os.getenv("CONDA_PREFIX") .. "/bin/python" } } },
-        ruff_lsp = { init_options = { settings = { lint = { args = { "--unsafe-fixes" } } } } },
       },
     },
   },
