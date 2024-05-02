f518e8e153da02a70c15f97982b32e8790c4939d
Menno <menno1337@gmail.com>
2024-05-02 14:43:23 +0300

    feat: add verbose flag to `pytest`
    Also cleans up some unused arguments.

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -3,13 +3,11 @@ return {
   opts = {
     adapters = {
       ["neotest-python"] = {
-        dap = { justMyCode = false },
-        args = { "--log-level", "DEBUG" },
+        args = { "--log-level", "DEBUG", "-v" },
         runner = "pytest",
         python = function()
           return require("venv-selector").get_active_path() or os.getenv("CONDA_PREFIX") .. "/bin/python"
         end,
-        pytest_discover_instances = true,
       },
     },
   },
