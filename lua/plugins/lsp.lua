8c76584926d7d6affbab5b525a57a635aa125dad
Menno <menno1337@gmail.com>
2024-05-02 14:41:17 +0300

    chore: ignore type warnings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,9 +1,7 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
     opts = {
-      ---@type lspconfig.options
       servers = {
         -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = { settings = { python = { pythonPath = os.getenv("CONDA_PREFIX") .. "/bin/python" } } },
