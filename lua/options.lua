1d22d0b15e48ccecfbfe015cf186b45434cc4423
Menno <menno1337@gmail.com>
2024-06-27 00:09:38 +0200

    refactor: move indent options to `after/ftplugin`

--- lua/options.lua
+++ lua/options.lua
@@ -3,13 +3,6 @@ vim.g.maplocalleader = "\\"
 
 local opt = vim.opt
 
--- Indent rules
-opt.shiftwidth = 4 -- Size of an indent
-opt.tabstop = 4 -- Number of spaces tabs count for
-opt.smartindent = true -- Insert indents automatically
-opt.expandtab = true -- Use spaces instead of tabs
-opt.shiftround = true -- Round indent
-
 -- Cursor
 opt.number = true -- Print line number
 opt.relativenumber = true -- Relative line numbers
@@ -18,9 +11,7 @@ opt.scrolloff = 4 -- Lines of context
 opt.colorcolumn = "80" -- Ruler
 
 -- Visual
-opt.termguicolors = true -- True color support
 opt.list = true -- Show some invisible characters (tabs...
-opt.wrap = false
 
 -- System
 opt.clipboard = "unnamedplus" -- Sync with system clipboard
