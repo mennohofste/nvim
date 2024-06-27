cbe423438ce3b6db8e95dea4778ef237ae7e7fff
Menno <menno1337@gmail.com>
2024-06-27 07:44:59 +0200

    refactor: clean up and document `mini.ai` custom objects

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -7,24 +7,19 @@ return {
     ai.setup({
       n_lines = 500,
       custom_textobjects = {
+        -- Object
         o = ai.gen_spec.treesitter({
           a = { "@block.outer", "@conditional.outer", "@loop.outer" },
           i = { "@block.inner", "@conditional.inner", "@loop.inner" },
         }, {}),
+        -- Function
         f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+        -- Class
         c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
-        t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
-        d = { "%f[%d]%d+" }, -- digits
-        e = { -- Word with case
-          {
-            "%u[%l%d]+%f[^%l%d]",
-            "%f[%S][%l%d]+%f[^%l%d]",
-            "%f[%P][%l%d]+%f[^%l%d]",
-            "^[%l%d]+%f[^%l%d]",
-          },
-          "^().*()$",
-        },
-        g = function() -- Whole buffer, similar to `gg` and 'G' motion
+        -- Digit
+        d = { "%f[%d]%d+" },
+        -- Global, whole buffer like `gg` and `G`
+        g = function()
           local from = { line = 1, col = 1 }
           local to = {
             line = vim.fn.line("$"),
@@ -32,8 +27,6 @@ return {
           }
           return { from = from, to = to }
         end,
-        u = ai.gen_spec.function_call(), -- u for "Usage"
-        U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
       },
     })
 
