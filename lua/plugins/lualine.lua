336af79a4f29b116b83652456b464e6c83417698
Menno <menno@sensofusion.com>
2024-11-28 15:18:39 +0200

    refactor: simplify lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,20 +1,4 @@
 return {
   "nvim-lualine/lualine.nvim",
-  opts = {
-    sections = {
-      lualine_x = {
-        "filetype",
-        -- stylua: ignore
-        {
-          function() return require("noice").api.status.mode.get() end,
-          cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-        },
-      },
-    },
-  },
-  config = function(_, opts)
-    -- Do not show mode since lualine already shows status
-    vim.opt.showmode = false
-    require("lualine").setup(opts)
-  end,
+  opts = {},
 }
