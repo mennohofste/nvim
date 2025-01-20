5f50eeb1b970936eab4b999317275ad927ba1b54
Menno <menno@sensofusion.com>
2025-01-20 12:41:45 +0200

    refactor: simplify noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -12,49 +12,10 @@ return {
         ["cmp.entry.get_documentation"] = true,
       },
     },
-    routes = {
-      {
-        filter = {
-          event = "msg_show",
-          any = {
-            { find = "%d+L, %d+B" },
-            { find = "; after #%d+" },
-            { find = "; before #%d+" },
-          },
-        },
-        view = "mini",
-      },
-    },
     presets = {
       command_palette = true,
       long_message_to_split = true,
       lsp_doc_border = true,
-    },
-  },
-  keys = {
-    {
-      "<c-f>",
-      function()
-        if not require("noice.lsp").scroll(4) then
-          return "<c-f>"
-        end
-      end,
-      silent = true,
-      expr = true,
-      desc = "Scroll Forward",
-      mode = { "i", "n", "s" },
-    },
-    {
-      "<c-b>",
-      function()
-        if not require("noice.lsp").scroll(-4) then
-          return "<c-b>"
-        end
-      end,
-      silent = true,
-      expr = true,
-      desc = "Scroll Backward",
-      mode = { "i", "n", "s" },
     },
   },
 }
