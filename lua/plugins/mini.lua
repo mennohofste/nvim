008e495c1f2209945af511b390d1468c3cffeaea
Menno <menno@sensofusion.com>
2025-03-03 10:56:23 +0200

    fix: increase textobjects search from 50 to 500
    This allows using "yac" for classes that are up to size 500 instead of
    50. There are not that many classes of 50 lines.

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -5,6 +5,7 @@ return {
   config = function()
     local spec_treesitter = require("mini.ai").gen_spec.treesitter
     require("mini.ai").setup({
+      n_lines = 500,
       custom_textobjects = {
         f = spec_treesitter({ a = "@function.outer", i = "@function.inner" }),
         c = spec_treesitter({ a = "@class.outer", i = "@class.inner" }),
