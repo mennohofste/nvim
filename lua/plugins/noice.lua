163365fb1da2203d5976e1159db2a80ac8edfd91
Menno <menno@sensofusion.com>
2025-04-02 22:33:25 +0300

    refactor: center noice and check health

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -6,8 +6,13 @@ return {
   ---@type NoiceConfig
   ---@diagnostic disable-next-line: missing-fields
   opts = {
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+      },
+    },
     presets = {
-      command_palette = true,
       long_message_to_split = true,
       lsp_doc_border = true,
     },
