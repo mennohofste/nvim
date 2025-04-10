091d74d0c9191cf19a734eb2e90e5be0c8ea8f9c
Menno <menno@sensofusion.com>
2025-04-10 14:12:45 +0300

    feat: add copilot

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,14 +1,40 @@
 return {
-  "saghen/blink.cmp",
-  version = "*",
-  dependencies = "rafamadriz/friendly-snippets",
-  ---@module "blink.cmp"
-  ---@type blink.cmp.Config
-  opts = {
-    completion = {
-      menu = { draw = { treesitter = { "lsp" } } },
-      documentation = { auto_show = true },
-      ghost_text = { enabled = true },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "BufReadPost",
+    opts = {
+      panel = { enabled = false },
+      suggestion = { enabled = false },
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    version = "*",
+    dependencies = {
+      "giuxtaposition/blink-cmp-copilot",
+      "rafamadriz/friendly-snippets",
+    },
+    ---@module "blink.cmp"
+    ---@type blink.cmp.Config
+    opts = {
+      completion = {
+        menu = { draw = { treesitter = { "lsp" } } },
+        documentation = { auto_show = true },
+        ghost_text = { enabled = true },
+      },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer", "copilot" },
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-cmp-copilot",
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
     },
   },
 }
