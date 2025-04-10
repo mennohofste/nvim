97040c556db920b94a450bc9848695b9ea14e7fd
Menno <menno@sensofusion.com>
2025-04-10 14:08:07 +0300

    feat: add blink options

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -4,5 +4,11 @@ return {
   dependencies = "rafamadriz/friendly-snippets",
   ---@module "blink.cmp"
   ---@type blink.cmp.Config
-  opts = {},
+  opts = {
+    completion = {
+      menu = { draw = { treesitter = { "lsp" } } },
+      documentation = { auto_show = true },
+      ghost_text = { enabled = true },
+    },
+  },
 }
