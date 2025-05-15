f7041bb292c4acea4adc5f5a575d504e932e8cb2
Menno <menno@sensofusion.com>
2025-05-15 13:40:02 +0300

    feat: add avante for LLM chat

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -10,6 +10,18 @@ return {
     },
   },
   {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    version = false,
+    opts = { provider = "copilot" },
+    build = "make",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+  },
+  {
     "saghen/blink.cmp",
     version = "*",
     dependencies = {
