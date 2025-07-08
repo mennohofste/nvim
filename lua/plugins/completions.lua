c1d7248f8544032045b731f8911c11581b810b2e
Menno Hofste <menno@sensofusion.com>
2025-07-08 19:47:58 +0300

    chore: remove unused completion provider

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -13,25 +13,7 @@ return {
     "yetone/avante.nvim",
     event = "VeryLazy",
     version = false,
-    opts = {
-      provider = "qwen3",
-      vendors = {
-        qwen3 = {
-          __inherited_from = "openai",
-          api_key_name = "SENSO_QWEN3_API_KEY",
-          endpoint = "https://gpu.sensofusion.com:8200/v1",
-          model = "Qwen/Qwen3-235B-A22B",
-          disable_tools = true,
-        },
-        qwen25 = {
-          __inherited_from = "openai",
-          api_key_name = "SENSO_QWEN25_API_KEY",
-          endpoint = "https://gpu.sensofusion.com:8100/v1",
-          model = "Qwen/Qwen2.5-Coder-32B-Instruct",
-          disable_tools = true,
-        },
-      },
-    },
+    opts = {},
     build = "make",
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
@@ -47,8 +29,6 @@ return {
       "Kaiser-Yang/blink-cmp-avante",
       "rafamadriz/friendly-snippets",
     },
-    ---@module "blink.cmp"
-    ---@type blink.cmp.Config
     opts = {
       completion = {
         menu = { draw = { treesitter = { "lsp" } } },
