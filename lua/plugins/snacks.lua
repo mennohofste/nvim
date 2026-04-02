29fdd2eb3de045030bdc24ae9a2c0aa1ddd09f69
Menno Hofste <menno@sensofusion.com>
2026-04-02 17:55:41 +0300

    revert: use mini.files over snacks.explorer

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,7 +6,6 @@ return {
   ---@type snacks.Config
   opts = {
     dashboard = { enabled = true },
-    explorer = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true },
     notifier = { enabled = true },
@@ -18,8 +17,6 @@ return {
     { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
     { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
     { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
-
-    { "<leader>e", function() Snacks.explorer() end, desc = "Explorer" },
 
     { "<leader>f", function() Snacks.picker.files() end, desc = "Find Files" },
     { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
