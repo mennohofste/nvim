1abee7ad09a7689760635f4afe5ecb4537e06622
Menno Hofste <menno@sensofusion.com>
2026-08-30 20:13:04 +0300

    chore: disable wrap

--- lua/options.lua
+++ lua/options.lua
@@ -1,28 +1,20 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-local opt = vim.opt
-
--- Cursor
-opt.number = true -- Print line number
-opt.relativenumber = true -- Relative line numbers
-opt.cursorline = true -- Enable highlighting of the current line
-opt.scrolloff = 4 -- Lines of context
-opt.colorcolumn = "80" -- Ruler
-
--- Visual
-opt.list = true -- Show some invisible characters (tabs...
-opt.signcolumn = "yes" -- Always show sign column to prevent text jumping
-
--- System
-opt.clipboard = "unnamedplus" -- Sync with system clipboard
-opt.confirm = true -- Confirm to before exiting modified buffer
-opt.undofile = true
-opt.undolevels = 10000
-
--- Search
-opt.ignorecase = true
-opt.smartcase = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 4
+vim.opt.colorcolumn = "80"
+vim.opt.list = true
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.confirm = true
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 -- Diagnostic
 vim.diagnostic.config({ virtual_lines = { current_line = true } })
